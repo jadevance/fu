03c6d9d9a8edc943e3531283fe674566635ae62f
Jade Vance <jadenicolevance@gmail.com>
2016-08-08 13:53:43 -0700

    added a sign in with google button to the main storyboard as well as an app load screen. Added in more oauth code plus commented out section that will eventually hit my backend API

--- Fuzz Therapy/AppDelegate.swift
+++ Fuzz Therapy/AppDelegate.swift
@@ -5,20 +5,76 @@
 //  Created by Jade Vance on 8/7/16.
 //  Copyright © 2016 Jade Vance. All rights reserved.
 //
-
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 
     var window: UIWindow?
-
-
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+    
+    func application(application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Initialize sign-in
+        var configureError: NSError?
+        GGLContext.sharedInstance().configureWithError(&configureError)
+        assert(configureError == nil, "Error configuring Google services: \(configureError)")
+        
+        GIDSignIn.sharedInstance().delegate = self
+        
         return true
     }
+    
+    func application(application: UIApplication,
+                     openURL url: NSURL, options: [String: AnyObject]) -> Bool {
+        return GIDSignIn.sharedInstance().handleURL(url,
+                                                    sourceApplication: options[UIApplicationOpenURLOptionsSourceApplicationKey] as? String,
+                                                    annotation: options[UIApplicationOpenURLOptionsAnnotationKey])
+    }
+    
+    func signIn(signIn: GIDSignIn!, didSignInForUser user: GIDGoogleUser!,
+                withError error: NSError!) {
+        if (error == nil) {
+            // Perform any operations on signed in user here.
+            let userId = user.userID                  // For client-side use only!
+            let idToken = user.authentication.idToken // Safe to send to the server
+            let fullName = user.profile.name
+            let givenName = user.profile.givenName
+            let familyName = user.profile.familyName
+            let email = user.profile.email
+            
+            // get the user token , sign in to backend API
+//            NSString idToken = user.authentication.idToken
+//            NSString signinEndpoint = @"https://fuzztherapy.com/user/signin";
+//            NSDictionary *params = @{@"idtoken": idToken};
+//            
+//            NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:signinEndpoint];
+//            [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+//            [request setHTTPMethod:@"POST"];
+//            [request setHTTPBody:[self httpBodyForParamsDictionary:params]];
+//            
+//            NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+//            [NSURLConnection sendAsynchronousRequest:request
+//            queue:queue
+//            completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
+//            if (error) {
+//            NSLog(@"Error: %@", error.localizedDescription);
+//            } else {
+//            NSLog(@"Signed in as %@", data.bytes);
+//            }
+//            }];
+            
+        } else {
+            print("\(error.localizedDescription)")
+        }
+    }
+    
+    func signIn(signIn: GIDSignIn!, didDisconnectWithUser user:GIDGoogleUser!,
+                withError error: NSError!) {
+        // Perform any operations when the user disconnects from app here.
+        // ...
+    }
 
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
