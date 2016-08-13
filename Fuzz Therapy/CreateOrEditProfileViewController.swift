30d13bc9553974e738d94ea7a5804fa32ca7e6e0
Jade Vance <jadenicolevance@gmail.com>
2016-08-13 11:50:50 -0700

    Started working on the puppy matching view. Added in logic to handle accessing photos on the device. Accessing the camera does not work yet though

--- Fuzz Therapy/CreateOrEditProfileViewController.swift
+++ Fuzz Therapy/CreateOrEditProfileViewController.swift
@@ -14,31 +14,32 @@ import UIKit
 // Match the ObjC symbol name inside Main.storyboard.
 @objc(CreateOrEditViewController)
 
-class CreateOrEditViewController: UIViewController {
+class CreateOrEditViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var locationField: UITextField!
+    @IBOutlet weak var availabilityField: UITextField!
+    @IBOutlet weak var dogNameField: UITextField!
+    @IBOutlet weak var dogBreedField: UITextField!
+    @IBOutlet weak var dogAgeField: UITextField!
 
+    @IBOutlet weak var imagePicked: UIImageView!
     
-    
-        @IBOutlet weak var nameField: UITextField!
-        @IBOutlet weak var locationField: UITextField!
-        @IBOutlet weak var availabilityField: UITextField!
-        @IBOutlet weak var dogNameField: UITextField!
-        @IBOutlet weak var dogBreedField: UITextField!
-        @IBOutlet weak var dogAgeField: UITextField!
-    
-        @IBOutlet weak var addPhotoButton: UIButton!
-        @IBOutlet weak var saveProfileButton: UIButton!
+    @IBOutlet weak var saveProfile: UIButton!
+    @IBOutlet weak var addPhoto: UIButton!
+    @IBOutlet weak var photoLibrary: UIButton!
 
     override func viewDidLoad() {
     
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func onTextFieldDoneEditing(sender: UITextField){sender.resignFirstResponder()
     }
-    
     
     @IBAction func onTapGestureRecognized(sender: AnyObject) {
         nameField.resignFirstResponder()
@@ -48,6 +49,54 @@ class CreateOrEditViewController: UIViewController {
         dogBreedField.resignFirstResponder()
         dogAgeField.resignFirstResponder()
     }
+    
+    
+    @IBAction func onTakePhotoPressed(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.Camera;
+            imagePicker.allowsEditing = false
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
 
+    @IBAction func onPhotoLibraryPressed(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary;
+            imagePicker.allowsEditing = true
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        imagePicked.image = image
+        self.dismissViewControllerAnimated(true, completion: nil);
+    }
 
+    @IBAction func onSaveButtonPressed(sender: AnyObject) {
+        let imageData = UIImageJPEGRepresentation(imagePicked.image!, 0.6)
+        var compressedJPGImage = UIImage(data: imageData!)
+//        UIImageWriteToSavedPhotosAlbum(compressedJPGImage, nil, nil, nil)
+        // package data
+        // fire off callback to API
+        // if successful do this:
+            let alertController = UIAlertController(title: "Yay!", message: "Your profile has been saved.", preferredStyle: .Alert)
+        
+            let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+            alertController.addAction(defaultAction)
+        
+            presentViewController(alertController, animated: true, completion: nil)
+        // else if unsuccessful
+//            let alertController = UIAlertController(title: "Oh no!!", message: "Your profile has NOT been saved.", preferredStyle: .Alert)
+//        
+//            let defaultAction = UIAlertAction(title: "Try again?", style: .Default, handler: nil)
+//            alertController.addAction(defaultAction)
+//        
+//            presentViewController(alertController, animated: true, completion: nil)
+        
+    }
 }
---
+++ Fuzz Therapy/PuppyPalsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PuppyPalsViewController.swift
+//  Fuzz Therapy
+//
+//  Created by Jade Vance on 8/13/16.
+//  Copyright © 2016 Jade Vance. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PuppyPalsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
